6d392cdfa4fcd4784a6359db07bd6e67ba31c9a8
CraftOLeague <stavzog@gmail.com>
2018-01-31 18:37:47 +0200

    Update MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -127,7 +127,7 @@ async def on_message(message):
             num = math.floor(randint(1,9))
             randnums.append(num)
         randnums = str(randnums)
-        await client.send_message(message.channel, "``` \n Random Numbers: %s \n ```" % (" ".join(randnums)))
+        await client.send_message(message.channel, "``` \n Random Numbers: %s \n ```" % (" ".join(randnums[0])))
     if message.content.upper().startswith(":CODE"):
         args = message.content.split(" ")
         embed=discord.Embed(description="%s" % (" ".join(args[1:])), color=0x008080)
