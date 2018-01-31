5137774c90be358d6505cfda89ddbaad70972fab
CraftOLeague <stavzog@gmail.com>
2018-01-31 18:31:48 +0200

    Update MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -12,7 +12,7 @@ client = commands.Bot(command_prefix = ":")
 @client.event
 async def on_ready():
     print("Bot is running! GG")
-    await client.change_status(game=discord.Game(name='with :help'))
+    await client.change_presence(game=discord.Game(name='with :help'))
 
 @client.event
 async def on_message(message):
@@ -127,7 +127,7 @@ async def on_message(message):
             num = math.floor(randint(1,9))
             randnums.append(num)
         randnums = str(randnums)
-        await client.send_message(message.channel, "```css \n Random Numbers: %s \n ```" % (" ".join(randnums)))
+        await client.send_message(message.channel, "``` \n Random Numbers: %s \n ```" % (" ".join(randnums)))
     if message.content.upper().startswith(":CODE"):
         args = message.content.split(" ")
         embed=discord.Embed(description="%s" % (" ".join(args[1:])), color=0x008080)
