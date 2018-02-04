f320e54db9974b27490d9fa2f9ad6d3bcd08905c
CraftOLeague <stavzog@gmail.com>
2018-02-04 12:04:55 +0200

    Update optional.py

--- optional.py
+++ optional.py
@@ -3,9 +3,8 @@ from discord.ext import commands
 
 class Extra():
     """Extra Bot Commands"""
-	def __init__(self,bot):
-        self.bot = bot
-    
-    
-      
-  
+	def __init__(self, bot):
+		self.bot = bot
+		
+def setup(bot):
+	bot.add_cog(Extra(bot))
